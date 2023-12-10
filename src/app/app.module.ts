c81fbfbd6e30db36e5e2a051ea3c481fd1261561
Md.Shafiqur Rohman <60768951+ShafiqurRohman@users.noreply.github.com>
2023-12-10 23:02:28 +0600

    init-commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DescriptionComponent } from './description/description.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DescriptionComponent,
+    PrivacyPolicyComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
